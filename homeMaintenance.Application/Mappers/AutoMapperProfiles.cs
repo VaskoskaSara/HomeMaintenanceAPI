430056bd878164f39d00658d977fb02c5793c3d9
VSaraSI <saravaskoska11@hotmail.com>
2024-07-25 23:04:26 +0200

    mapper

--- homeMaintenance.Application/Mappers/AutoMapperProfiles.cs
+++ homeMaintenance.Application/Mappers/AutoMapperProfiles.cs
@@ -2,6 +2,7 @@
 using homeMaintenance.Application.Commands.UserLogin;
 using homeMaintenance.Application.Commands.UserRegistration;
 using homeMaintenance.Domain.Entities;
+using System.Globalization;
 
 namespace homeMaintenance.Application.Mappers
 {
@@ -9,12 +10,29 @@ namespace homeMaintenance.Application.Mappers
     {
         public AutoMapperProfiles()
         {
+            //CreateMap<UserRegistrationCommand, User>()
+            //    .ForMember(src => src.BirthDate, dest => dest.MapFrom(src => DateTime.Parse(src.BirthDate)))
+            //    .ReverseMap();
+
+            //CreateMap<User, UserRegistrationCommand>()
+            //   .ForMember(src => src.Avatar, dest => dest.Ignore())
+            //   .ForMember(src => src.Photos, dest => dest.Ignore())
+            //   .ReverseMap();
+
             CreateMap<UserRegistrationCommand, User>()
-                .ForMember(src => src.BirthDate, dest => dest.MapFrom(src => DateTime.Parse(src.BirthDate)))
-                .ReverseMap();
+            .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => ParseCustomDate(src.BirthDate)))
+           .ReverseMap()
+           .ForMember(dest => dest.Avatar, opt => opt.Ignore())
+           .ForMember(dest => dest.Photos, opt => opt.Ignore());
 
             CreateMap<UserLoginCommand, User>()
                 .ReverseMap();
         }
+
+        private DateTime ParseCustomDate(string date)
+        {
+            return DateTime.ParseExact(date, "ddd MMM dd yyyy HH:mm:ss 'GMT'K '(Central European Standard Time)'", CultureInfo.InvariantCulture);
+        }
     }
+
 }
